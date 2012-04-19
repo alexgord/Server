86b74db735555f7dd8c3adf07bb7adbf3db84c94
cmdrkeen01 <peter.johnston92@gmail.com>
2012-04-18 20:43:08 -0400

    now echos

--- A4 Server/src/ServerApp.java
+++ A4 Server/src/ServerApp.java
@@ -1,8 +1,12 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 import javax.xml.ws.http.HTTPException;
 
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 
 public class ServerApp extends EchoServer {
@@ -15,8 +19,15 @@ public class ServerApp extends EchoServer {
 	public static void main(String[] args) throws HTTPException, IOException {
 		// TODO Auto-generated method stub
 		//HttpExchange exchange = new HttpExchange();
-		//handle h = new handle;
+		EchoServer echo = new EchoServer();
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+	    server.createContext("/test", new EchoServer());
+	    server.setExecutor(null); // creates a default executor
+	    server.start();
 		//handle(exchange);
+		
+		HttpExchange exchange = null;
+		//echo.handle(exchange);
 	}
 
 }
